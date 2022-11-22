dbdfd11d3699fef4fa09976123e5db75b1b20e3c
Abhishek Kasaundhan <abhishek2001ckt@gmail.com>
2022-11-22 16:29:07 +0530

    .

--- Module_4th_solution/script.js
+++ Module_4th_solution/script.js
@@ -1,14 +1,14 @@
 var person_name=new Array();
-person_name[0]="Yachika";
-person_name[1]="Johnny";
-person_name[2]="Jenshi";
+person_name[0]="Yaakov";
+person_name[1]="John";
+person_name[2]="Jen";
 person_name[3]="jason";
 person_name[4]="paul";
-person_name[5]="franky";
-person_name[6]="larrow";
-person_name[7]="paul";
+person_name[5]="frank";
+person_name[6]="larry";
+person_name[7]="paula";
 person_name[8]="laura";
-person_name[9]="jimmy";
+person_name[9]="jim";
 
 
 for (var i = 0; i < person_name.length; i++) {
